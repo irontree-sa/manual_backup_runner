3d5786762d4d046ba8b92d79bea61118c730b3a1
Nicholas Keene <nicholas@irontree.co.za>
2026-08-30 16:49:07 +0200

    Add System.Text.Json.Serialization using directive to fix serializer import

--- src/AcronisBackupTrigger/ConfigurationStore.cs
+++ src/AcronisBackupTrigger/ConfigurationStore.cs
@@ -3,6 +3,7 @@ using System.Security.AccessControl;
 using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace AcronisBackupTrigger;
 
